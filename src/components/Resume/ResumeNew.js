66c7ce68b77290605d4d9cda16023553d3e1960c
Rashmi1526 <rashmi.19b101039@abes.ac.in>
2022-07-21 22:52:48 +0530

    added resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/rashmi-resume-off.pdf";
+import pdf from "../../Assets/../Assets/Rashmi-singh-Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/Rashmi1526/online-resume/main/rashmi-resume-off.pdf";
+  "https://raw.githubusercontent.com/Rashmi1526/online-resume/main/Rashmi-singh-Resume.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
