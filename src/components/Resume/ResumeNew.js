3defd047a1c5bf7fa0c1900eddb8fcb85e3da07e
Rashmi Singh <71384066+Rashmi1526@users.noreply.github.com>
2025-04-11 17:45:33 +0530

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/Rashmi1526/online-resume/main/Rashmi-singh-Resume.pdf";
+  "https://raw.githubusercontent.com/Rashmi1526/online-resume/main/RashmiSingh-Resume.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
